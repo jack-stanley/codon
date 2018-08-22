535b835ff1a93e2e9a2c908ff09de94e55395c46
Jack Stanley <jackstanley1000@gmail.com>
2018-08-21 21:45:31 -0700

    dffgdf

--- flask_app/articles/forms.py
+++ flask_app/articles/forms.py
@@ -6,8 +6,8 @@ from flask_wtf.recaptcha import RecaptchaField
 
 class ArticleForm(FlaskForm):
     title = StringField("Title*", validators = [DataRequired(), Length(max  = 200)])
-    content = TinyMceField("Content (autosaves every 10 seconds)", tinymce_options = {"plugins": "autosave fullscreen hr preview insertdatetime code link print searchreplace pagebreak textcolor wordcount", "width": "80%", "autosave_interval": "10s", "menubar": "file edit format view insert tools",
-    "toolbar": "undo redo | styleselect forecolor backcolor | fontselect fontsizeselect | bold italic underline strikethrough | alignleft, aligncenter, alignright, alignjustify | bullist, numlist, outdent, indent, blockquote | fullscreen preview", "browser_spellcheck": True})
+    content = TinyMceField("Content (autosaves every 10 seconds)", tinymce_options = {"plugins": "autosave fullscreen hr preview insertdatetime code link print searchreplace pagebreak textcolor wordcount image contextmenu table autolink", "width": "80%", "autosave_interval": "10s", "menubar": "file edit format view insert table tools",
+    "toolbar": "undo redo | styleselect forecolor backcolor | fontsizeselect | bold italic underline strikethrough | alignleft, aligncenter, alignright, alignjustify | bullist, numlist, outdent, indent, blockquote | fullscreen preview", "browser_spellcheck": True, "contextmenu": "true copy cut paste | link image inserttable | cell row column deletetable",
+    "content_css": "content.css", "body_class": "content_editor"})
     section = SelectField("Section*", choices = [("Introduction", "Introduction"), ("Main", "Main"), ("Resources", "Resources"), ("Miscellaneous", "Miscellaneous")])
-    recaptcha = RecaptchaField()
     submit = SubmitField("Post")
