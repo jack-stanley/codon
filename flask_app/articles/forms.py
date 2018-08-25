52d1d6ad340c7500587e699082648151c1d6b916
Jack Stanley <jackstanley1000@gmail.com>
2018-08-24 20:11:10 -0700

    dfsds

--- flask_app/articles/forms.py
+++ flask_app/articles/forms.py
@@ -8,6 +8,6 @@ class ArticleForm(FlaskForm):
     title = StringField("Title*", validators = [DataRequired(), Length(max  = 200)])
     content = TinyMceField("Content (autosaves every 10 seconds)", tinymce_options = {"plugins": "autosave fullscreen hr preview insertdatetime code link print searchreplace pagebreak textcolor wordcount image contextmenu table autolink", "width": "80%", "autosave_interval": "10s", "menubar": "file edit format view insert table tools",
     "toolbar": "undo redo | styleselect forecolor backcolor | fontsizeselect | bold italic underline strikethrough | alignleft, aligncenter, alignright, alignjustify | bullist, numlist, outdent, indent, blockquote | fullscreen preview", "browser_spellcheck": True, "contextmenu": "true copy cut paste | link image inserttable | cell row column deletetable",
-    "content_css": "content.css", "body_class": "content_editor"})
+    "content_css": "content.css", "body_class": "content_editor", "image_caption": True})
     section = SelectField("Section*", choices = [("Introduction", "Introduction"), ("Methods and Materials", "Methods and Materials"), ("Results", "Results"), ("Discussion and Conclusion", "Discussion and Conclusion"), ("Resources", "Resources"), ("References", "References"), ("Miscellaneous", "Miscellaneous")])
     submit = SubmitField("Post")
